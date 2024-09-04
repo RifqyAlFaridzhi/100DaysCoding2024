b452a3c4680c12aeef66bb15d160f628143701ef
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-09-03 17:46:16 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package pkg100haringoding2;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        
+        System.out.println("NAMA  : Rifqy al faridzhi");
+        System.out.println("NIM   : D0224505");
+        System.out.println("KELAS : Informatika c");
+    }
+    
+}
