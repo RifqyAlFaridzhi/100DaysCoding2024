7c871d9cbf1288aeb4a2ff3e1c560ca404a57904
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-09-08 03:55:34 -0700

    Add files via upload

---
+++ Day07.java
@@ -0,0 +1,17 @@
+package pkg100haringoding2;
+
+public class Day07 {
+    
+    public static void main(String[] args) {
+        //variable  
+        String Nama = "Rifqy al faridzhi";
+        char Kelas = 'c';
+        int Umur = 19;
+        
+        //print out
+        System.out.println("Nama:" + Nama);
+        System.out.print("Kelas:"+ Kelas);
+        System.out.printf("\nUmur:%d tahun", Umur);
+        
+    }
+}
