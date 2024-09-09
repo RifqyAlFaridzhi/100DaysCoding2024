aa0a37d91bbf518038a742b3284fcfb416bec0eb
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-09-08 22:06:42 -0700

    Add files via upload

---
+++ Day08.java
@@ -0,0 +1,29 @@
+package pkg100haringoding2;
+
+public class Day08{
+    
+    public static void main(String[] args) {
+        //variable
+        String Nama = "Rifqy al faridzhi";
+        char Generasi = 'z';
+        int Umur = 19;
+        /*sistem print out*/
+        System.out.println("Nama  :"+Nama);
+        System.out.print("Generasi: "+Generasi);
+        System.out.printf("\nUmur: %s",Umur);
+        /*Line*/
+        System.out.println("");
+        System.out.println("");
+        System.out.printf("[======================]");
+        System.out.println("");
+        System.out.println("");
+        //variable update
+        Nama = "Call me Ari";
+        Generasi = 'A';
+        Umur = 24;
+        /*sistem print out variable update*/
+        System.out.println("Nama: "+Nama);
+        System.out.print("Generasi: "+Generasi);
+        System.out.printf("\nUmur: %s", Umur);
+    }
+}
