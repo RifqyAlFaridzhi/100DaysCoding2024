225f8e2865e47bd01480a7b6e1b2820bba4edad3
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-09-09 21:51:26 -0700

    Add files via upload

---
+++ Day09.java
@@ -0,0 +1,15 @@
+package pkg100haringoding2;
+
+public class Day09 {
+    
+    public static void main(String[] args) {
+        //variable conts/constanta atau variable mutlak
+        final String nama = "Call Me Ari";
+        final int tinggiBadan = 170;
+        final byte umur = 19;
+        //print out
+        System.out.println("Nama: "+nama);
+        System.out.print("TinggiBadan: "+tinggiBadan);
+        System.out.printf("\nUmur: %s",umur);
+    }
+}
