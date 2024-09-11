9f9a93545b4a1eb45465fd0ff55eb463ae62b840
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-09-11 00:45:24 -0700

    Update Day10.java

--- Day10.java
+++ Day10.java
@@ -3,7 +3,7 @@ package pkg100haringoding2;
 public class Day10 {
     
     public static void main(String[] args) {
-        //variable byte(tipe data angka yang mecakup -128 sampai 127)
+        //variable byte(tipe data angka yang mengcakup -128 sampai 127)
         byte jumlahData = 35;
         System.out.println("Jumlah Data Yang Dikumpulkan: "+ jumlahData);
     }
