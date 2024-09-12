56d2ddd09fe0586a6eca1c8dfe5091fc7f0f7667
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-09-12 00:28:56 -0700

    Add files via upload

---
+++ Day11.java
@@ -0,0 +1,11 @@
+package pkg100haringoding2;
+
+public class Day11 {
+    
+    public static void main(String[] args) {
+        //variable short(ialah tipe data angka yang mencakup -32,768 sampai 32,767.)
+        short jumlahData = 32450;
+        System.out.println("Jumlah Data Yang Dikumpulkan: "+ jumlahData);
+
+    }
+}
