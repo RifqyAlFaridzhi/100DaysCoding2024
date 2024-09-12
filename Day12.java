fbcc221a893cacd1ef748d5f7d69c01ffe9ca509
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-09-12 09:35:50 -0700

    Add files via upload

---
+++ Day12.java
@@ -0,0 +1,12 @@
+package pkg100haringoding2;
+
+public class Day12 {
+    
+    public static void main(String[] args) {
+        /*variable int (tipe data ini digunakan untuk menyimpan nilai bilangan bulat.Rentang 
+        nilai yang dapat disimpan adalah -2,147,483,648 sampai 2,147,483,647.)*/
+        int hasilData;
+        hasilData = 2340000;
+        System.out.println("semua hasil data: "+hasilData);        
+    }
+}
