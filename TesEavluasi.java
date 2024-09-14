03c126aa24ada84a69304cc8d7ca62e529606137
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-09-14 06:05:05 -0700

    Add files via upload

---
+++ TesEavluasi.java
@@ -0,0 +1,16 @@
+package teseavluasi;
+
+public class TesEavluasi {
+
+    public static void main(String[] args) {
+        String nama = "RIFQY AL FARIDZHI";
+        String alamat = "MANDING POLEWALI";
+        int umur = 19;
+        
+        System.out.println("NAMA: " +nama);
+        System.out.print("ALAMAT: " +alamat);
+        System.out.printf("\nUMUR: %s", umur);
+         
+  }
+    
+}
