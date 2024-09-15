d68be33bd909b3cb1ab76d592b71dba9ad996364
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-09-14 21:16:07 -0700

    Add files via upload

---
+++ Day14.java
@@ -0,0 +1,12 @@
+package pkg100haringoding2;
+
+public class Day14 {
+    
+    public static void main(String[] args) {
+        /*variable int(tipe data ini digunakan untuk menyimpan nilai bilangan bulat yang sangat besar. 
+        Rentang nilai yang dapat disimpan adalah -9,223,372,036,854,775,808 
+        sampai 9,223,372,036,854,775,807.)*/
+        long totalHarga = 15000000;
+        System.out.printf("total harga keseluruhan: Rp %s",totalHarga);
+    }
+}
