c4fce713936fa49438a8d3f8e801155fcbc57249
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-09-16 03:36:23 -0700

    Add files via upload

---
+++ Day15.java
@@ -0,0 +1,11 @@
+package pkg100haringoding2;
+
+public class Day15 {
+    
+    public static void main(String[] args) {
+        /*variable double(tipe data ini digunakan untuk menyimpan nilai desimal dengan presisi ganda. 
+        Rentang nilai yang dapat disimpan adalah 4.9E-324 sampai 1.8E+308.)*/
+        double RataRataNilai = 80.58;
+        System.out.printf("Rata-rata nilai yang di dapat adalah: %s",RataRataNilai);
+    }
+}
