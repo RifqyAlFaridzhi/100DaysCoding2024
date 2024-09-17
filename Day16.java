2bdc13afed1e52ab31c684e7225e9a7111682ac4
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-09-17 03:35:13 -0700

    Add files via upload

---
+++ Day16.java
@@ -0,0 +1,15 @@
+package pkg100haringoding2;
+
+public class Day16 {
+    
+    public static void main(String[] args) {
+        /*variable float(tipe data ini digunakan untuk menyimpan nilai desimal dengan presisi tunggal.
+        Rentang nilai yang dapat disimpan adalah 1.4E-45 sampai 3.4E+38)*/
+        float a,b,c;
+        a = 30.54f;
+        b = 56.78f;
+        c = 80.45f;
+        float nilaiKeseluruhan = a+b+c;
+        System.out.println("nilai keseluruhan adalah: "+nilaiKeseluruhan);
+    }
+}
