285f176800d1e5e0840682709e7bd926b9f99de5
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-09-18 05:20:48 -0700

    Add files via upload

---
+++ Day17.java
@@ -0,0 +1,12 @@
+package pkg100haringoding2;
+
+public class Day17 {
+    
+    public static void main(String[] args) {
+        /*variable char(tipe data ini digunakan untuk menyimpan karakter.
+        Satu karakter dapat disimpan di dalam tipe data char.)*/
+        char JenisKelamin= 'L';
+        System.out.println("jenis kelamin: "+JenisKelamin);
+    
+    }
+}
