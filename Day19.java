42ee505238ab7b9afa8713165e181e146855808e
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-09-20 06:08:57 -0700

    Add files via upload

---
+++ Day19.java
@@ -0,0 +1,13 @@
+package pkg100haringoding2;
+
+public class Day19 {
+    
+ public static void main(String[] args) {
+      //tipe data referensi String
+      String nama = "Call me Ari";
+      String alamat = "Manding polewali, polewali mandar"; 
+      
+      System.out.println("nama\t: "+nama);
+      System.out.println("Alamat\t: "+alamat);
+    }
+}
