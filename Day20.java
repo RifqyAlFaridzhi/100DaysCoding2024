4d7f62ed175fc176bf08c2fd99fc72b2bcc45a0b
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-09-21 07:38:11 -0700

    Add files via upload

---
+++ Day20.java
@@ -0,0 +1,12 @@
+package pkg100haringoding2;
+
+public class Day20 {
+    
+    public static void main(String[] args) {
+        /*variable tipe data byte(tipe data angka yang menyimpan nilai bulangam kecil besaran bytenya
+          cuman 1 byte saja)*/
+          byte angka1 = 67;
+          angka1 += 35;
+          System.out.println("angka 1 sama dengan: "+angka1);
+    }
+}
