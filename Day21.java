ab60d86957dd4917b7f8fac5aa74370b1b34613d
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-09-22 06:02:58 -0700

    Add files via upload

---
+++ Day21.java
@@ -0,0 +1,13 @@
+package pkg100haringoding;
+
+public class Day21 {
+    
+    public static void main(String[] args) {
+        /*variable data short(tipe data ini digunakan untuk 
+        menyimpan nilai bilangan bulat yang lebih besar dari tipe data byte.)*/
+        short angka1 = 2340;
+        short angka2 = 3450;
+        short angka3 = 450;
+        System.out.println("hasil= "+angka1*angka2/angka3);
+    }
+}
