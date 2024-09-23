96463f31e3dc9839f3155b46497080d584447647
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-09-23 02:09:58 -0700

    Update Day22.java

--- Day22.java
+++ Day22.java
@@ -3,11 +3,11 @@ package pkg100haringoding;
 public class Day22 {
     
     public static void main(String[] args) {
-        /*variable data int(tipe data ini digunakan untuk menyimpan nilai bilangan bulat.
+        /*variable data integer(tipe data ini digunakan untuk menyimpan nilai bilangan bulat.
           besaran bytenya adalah 4 byte yang menampung angka hingga -2,147,483,648 sampai 2,147,483,647)  */
-        int angka1 = 9877;
-        int angka2 = 8799;
-        int angka3 = angka1+angka2;
+        Integer angka1 = 9877;
+        Integer angka2 = 8799;
+        Integer angka3 = angka1+angka2;
         angka3 += 900;
         angka3 *= 800;
         System.out.println("hasil semuanya adalah= "+angka3);    
