2dbf2f9a60fbdaa0a2f1835daf57153d44b031dc
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-09-23 00:16:18 -0700

    Add files via upload

---
+++ Day22.java
@@ -0,0 +1,15 @@
+package pkg100haringoding;
+
+public class Day22 {
+    
+    public static void main(String[] args) {
+        /*variable data int(tipe data ini digunakan untuk menyimpan nilai bilangan bulat.
+          besaran bytenya adalah 4 byte yang menampung angka hingga -2,147,483,648 sampai 2,147,483,647)  */
+        int angka1 = 9877;
+        int angka2 = 8799;
+        int angka3 = angka1+angka2;
+        angka3 += 900;
+        angka3 *= 800;
+        System.out.println("hasil semuanya adalah= "+angka3);    
+    }
+}
