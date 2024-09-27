9dbaa0db24d1470e87ca92329beb20e7a63aec84
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-09-27 06:45:41 -0700

    Add files via upload

---
+++ Day26.java
@@ -0,0 +1,12 @@
+package pkg100haringoding2;
+
+public class Day26 {
+    
+    public static void main(String[] args) {
+        /*tipe data ini digunakan untuk menyimpan karakter. Satu karakter dapat 
+        disimpan di dalam tipe data char. */
+
+        Character Myhuruf = 'S';
+        System.out.println("huruf favorit"+Myhuruf);
+    }
+}
