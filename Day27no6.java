78a750188c5edc20a3305d4cc358c5abb36408de
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-09-28 07:03:03 -0700

    Add files via upload

---
+++ Day27no6.java
@@ -0,0 +1,13 @@
+package pkg100haringoding2;
+
+public class Day27no6 {
+    
+    public static void main(String[] args) {
+        System.out.println("selamat datang di catatan data kebun binatang wild park!");
+        int jumlahhewan = 50;
+        String namahewan = "harimau";
+        final Byte umurhewan = 5; //variable ini consanta
+        namahewan = "singa";
+        System.out.printf("hewan bernama %s memilii umur %d tahun. jumlah hewan yang tersedia saat ini adalah %d", namahewan, umurhewan, jumlahhewan);
+    }
+}
