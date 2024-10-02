b988d15cfd7db552749513fe3303f15b985b95ee
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-10-02 05:25:04 -0700

    Add files via upload

---
+++ Day31.java
@@ -0,0 +1,24 @@
+package pkg100haringoding;
+
+public class Day31 {
+    
+    public static void main(String[] args) {
+        //Melakukan konversi tipe data String ke tipe data primitif
+        //metode yang di pakai bernama parsing
+
+        String A = "500"; //String ke long
+        long lngA = Integer.parseInt(A);
+            
+        System.out.println("hasil: " + lngA);
+
+        String B = "68.56"; //String ke float
+        float fltB = Float.parseFloat(B);
+
+        System.out.println("hasil: " + fltB);
+
+        String C = "true";
+        boolean bolnC = Boolean.parseBoolean(C);
+        System.out.println("hasil: " + bolnC);
+        
+    }
+}
