76ec648d6cbcb16922bdbc5a75e393989e297092
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-10-29 07:20:56 -0700

    Add files via upload

---
+++ Day58.java
@@ -0,0 +1,18 @@
+package pkg100haringoding;
+
+import java.util.Scanner;
+
+public class Day58 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("masukkan nilai: ");
+        int a = input.nextInt();
+
+        while (a > 0) {
+            System.out.print(a + " ");
+            a -= 2;
+        }
+
+    }
+}
