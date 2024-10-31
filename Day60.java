8a91ab50ff063ed54961c8255a838e2bb2703041
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-10-31 05:57:24 -0700

    Add files via upload

---
+++ Day60.java
@@ -0,0 +1,13 @@
+package pkg100haringoding2;
+
+public class Day60 {
+    
+    public static void main(String[] args) {
+        int i = 0;
+
+            do {
+              System.out.print(i + " ");
+              i++;  
+            } while (i <= 30);
+    }
+}
