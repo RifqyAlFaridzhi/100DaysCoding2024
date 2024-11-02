b2a73d3b9d3850b5d762b1b9119b2b4a5617625d
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-11-02 08:16:52 -0700

    Add files via upload

---
+++ Day62.java
@@ -0,0 +1,16 @@
+package pkg100haringoding2;
+import java.util.Scanner;
+
+public class Day62 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 1; i <= 30; i++) {            // penggunanaan break pada for
+            if (i == 25) {
+                break;  //berfungsi sebagai
+            }
+            System.out.println(i + " ");
+        }
+    }
+}
