6cbb38fe3d3621c7e52c7ded0814f53019b17aab
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-11-16 23:34:28 +0800

    Add files via upload

---
+++ Day76.java
@@ -0,0 +1,12 @@
+package pkg100haringoding2;
+import java.util.Scanner;
+
+public class Day76 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("masukkan kalimat: ");
+        String str1 = input.nextLine();
+        System.out.println(str1 + " jadi " + str1.toUpperCase());
+    }
+}
