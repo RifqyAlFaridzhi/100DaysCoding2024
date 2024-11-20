57fb2e855522ce3f0de074f096d6307525f582b7
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-11-20 21:03:06 +0800

    Add files via upload

---
+++ Day80.java
@@ -0,0 +1,10 @@
+package pkg100haringoding2;
+
+public class Day80 {
+    
+    public static void main(String[] args) {
+        String Str1 = "       nama saya ari         ";
+        System.out.println(Str1+ "no trim");
+        System.out.println(Str1.trim() + "\t\t memakai trim");           
+    }
+}
