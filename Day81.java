33a0050f2376a4ca4e276288cf4c8c2e02cb306d
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-11-21 21:25:40 +0800

    Add files via upload

---
+++ Day81.java
@@ -0,0 +1,16 @@
+package pkg100haringoding2;
+
+public class Day81 {
+    
+    public static void main(String[] args) {
+        int a = 90;
+        float b = 14.67f;
+        boolean c = true;
+        char d = 'a';
+
+        System.out.println("int ke String: " + String.valueOf(a));
+        System.out.println("float ke String: " + String.valueOf(b));
+        System.out.println("boolean ke String: " + String.valueOf(c));
+        System.out.println("char ke String: " + String.valueOf(d));
+    }
+}
