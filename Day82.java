6ca681df09303efc6526c4186019eac495f7d499
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-11-22 21:59:47 +0800

    Add files via upload

---
+++ Day82.java
@@ -0,0 +1,15 @@
+package pkg100haringoding2;
+
+public class Day82 {
+    
+    public static void main(String[] args) {
+        double Var1 = 19.89;
+        double Var2 = 18.76;
+        double Var3 = 23.56;
+        
+        System.out.println("angka yang di bulatkan atas: " + Math.ceil(Var1));
+        System.out.println("angka yang di bulatkan bawah: " + Math.floor(Var2));
+        System.out.println("angka yang di bulatkan salah satunya: " + Math.round(Var3));
+        
+    }
+}
