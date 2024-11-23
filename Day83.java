64491adc501c003b531f4799e01c8eb0e7a251eb
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-11-23 23:33:48 +0800

    Add files via upload

---
+++ Day83.java
@@ -0,0 +1,10 @@
+package pkg100haringoding2;
+import java.util.Arrays;
+
+public class Day83 {
+    
+    public static void main(String[] args) {
+        String[] Str1 = new String[] {"anakA" , "anakB" , "anakC", "anakD"}; 
+        System.out.println(Arrays.toString(Str1));
+    }
+}
