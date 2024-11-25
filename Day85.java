b5ca78b65ff2dd4782b403e16b3b2208f4e30885
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-11-25 23:06:15 +0800

    Add files via upload

---
+++ Day85.java
@@ -0,0 +1,10 @@
+package pkg100haringoding2;
+import java.util.Arrays;
+
+public class Day85 {
+    
+    public static void main(String[] args) {
+        int[] Str1 = new int[] {1, 2, 3, 4, 5};         //mengisi element array pada saat inisialisasi
+        System.out.println(Arrays.toString(Str1));
+    }
+}
