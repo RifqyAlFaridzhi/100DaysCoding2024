36f2391cc1ff83592a2cbe99c783dd62df14d1b8
RifqyAlFaridzhi <rifqyalfaridzhi@gmail.com>
2024-11-26 23:51:12 +0800

    Add files via upload

---
+++ Day86.java
@@ -0,0 +1,11 @@
+package pkg100haringoding2;
+
+public class Day86 {
+    
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4 , 10, 15};
+        for (int i : a) {
+            System.out.println("memanggil: " + i);
+        }
+    }
+}
